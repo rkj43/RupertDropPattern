d6228114c7aa4b2861b2603dccb185a14f96aa5d
Rahul <rahulkjohn43@gmail.com>
2023-09-05 22:00:18 +0100

    Banking PoC

---
+++ BankingSystemPoC/program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystemPoC
+{
+    public interface ILoggingService
+    {
+        void Log(string message);
+    }
+
+    public class LoggingService : ILoggingService
+    {
+        public void Log(string message)
+        {
+            // Simulate a potential failure in the logging service
+            if (new Random().Next(0, 5) == 0)
+            {
+                throw new Exception("Logging service failed!");
+            }
+            Console.WriteLine($"Log: {message}");
+        }
+    }
+
+    public class BankingSystem
+    {
+        private readonly ILoggingService _loggingService;
+
+        public BankingSystem(ILoggingService loggingService)
+        {
+            _loggingService = loggingService;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            // Core banking operation
+            Console.WriteLine($"Deposited: {amount}");
+
+            // Logging operation (sensitive component)
+            try
+            {
+                _loggingService.Log($"Deposited: {amount}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Logging failed, but banking operation succeeded. Error: {ex.Message}");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var loggingService = new LoggingService();
+            var bankingSystem = new BankingSystem(loggingService);
+
+            bankingSystem.Deposit(100);
+        }
+    }
+}
+
